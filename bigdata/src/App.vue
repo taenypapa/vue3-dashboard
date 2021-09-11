8afb456a532a4fc92360516b47528d864bf9ddeb
Seongho Lee <cylsh3452@hanwha.com>
2021-09-11 15:33:42 +0900

    first

---
+++ bigdata/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~@/../mdb/scss/index.free.scss';
+
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ bigdata/src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Dashboard</h1>
+  <div>
+    <!--<MonthlyChart />-->
+    <ColorTable />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import MonthlyChart from '../components/chart/MonthlyChart.vue'
+import ColorTable from '../components/table/ColorTable.vue'
+import { MDBContainer } from 'mdb-vue-ui-kit'
+
+@Options({
+  components: {
+    MonthlyChart,
+    MDBContainer,
+    ColorTable
+  }
+})
+export default class Dashboard extends Vue {}
+</script>
